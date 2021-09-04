d4ed311844a18f6b8438d1c31a627b483e60c30b
ret2src <2010034+ret2src@users.noreply.github.com>
2021-09-04 15:47:48 +0200

    Backup existing zshrc and make zsh default shell.

--- install.sh
+++ install.sh
@@ -23,9 +23,15 @@ if [ -f /etc/os-release ]; then
         unset ZSH
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-
         # Install powerlevel10k.
         git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+        # Backup zsh configuration.
+        mv ~/.zshrc ~/.zshrc.bak
+
+        # Make zsh the default shell.
+        chsh -s $(which zsh)
+        sudo chsh -s $(which zsh)
 
         echo "[*] Installation of dependencies finished. You can now link the dotfiles using the 'stow.sh' script."
     elif [[ "$NAME" =~ "Arch" ]]; then
