e1318528ab579dee9230b3add4de3be59fe3b6e4
ret2src <2010034+ret2src@users.noreply.github.com>
2021-09-11 13:45:43 +0200

    Add zsh-autosuggestions plugin.

--- install.sh
+++ install.sh
@@ -29,6 +29,9 @@ if [ -f /etc/os-release ]; then
         # Install powerlevel10k.
         git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+        # Install zsh-autosuggestions.
+        git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
         # Backup zsh configuration.
         mv ~/.zshrc ~/.zshrc.bak
 
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -77,7 +77,7 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git tmux safe-paste thefuck colored-man-pages command-not-found)
+plugins=(git tmux safe-paste thefuck colored-man-pages command-not-found zsh-autosuggestions)
 
 # Automatically attach to tmux.
 ZSH_TMUX_AUTOSTART=true
